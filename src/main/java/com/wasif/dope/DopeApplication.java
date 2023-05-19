14d5765b217d112980c1df11194c6bfc062194f5
WasifJavaBS1357 <wasif.islam@Brainstation-23.com>
2023-05-19 15:49:40 +0600

    initiate springboot mvc

---
+++ src/main/java/com/wasif/dope/DopeApplication.java
@@ -0,0 +1,13 @@
+package com.wasif.dope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wasif/dope/DopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasif.dope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
